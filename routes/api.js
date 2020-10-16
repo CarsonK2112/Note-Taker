190240d99b5d6a1cc21c76cd340f5dd32fc44fad
Red Dragon <carsonkeller1995@hotmail.com>
2020-10-16 11:41:01 -0500

    files moved out of develop folder

--- routes/api.js
+++ routes/api.js
@@ -2,18 +2,18 @@ var router =require("express").Router();
 var notes = require("../db/notes.js");
 
 
-router.get("/notes", function(req, res){
+router.get("/db/notes", function(req, res){
     notes.getNotes()
     .then(notes => res.json(notes))
     console.log(notes)
     .catch(err => res.status(500).json(err));
 })
-router.post("/notes", function(req, res){
+router.post("/db/notes", function(req, res){
     notes.addNotes(req.body)
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 })
-router.delete("/notes/:id", function(req, res){
+router.delete("/db/notes/:id", function(req, res){
     notes.removeNote(req.params.id)
     console.log(req.params.id)
     .then(() => res.json({ok: true}))
